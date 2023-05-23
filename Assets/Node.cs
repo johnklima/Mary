7a5153c16eb3fad88a94cdc7d99264ada45f368a
johnklima <klima.de.sa@gmail.com>
2023-05-23 14:11:49 +0100

    first nodes

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -7,6 +7,9 @@ public class Node : MonoBehaviour
 {
     public Text textObj;
     public string text;
+    public bool usable = true;
+
+    bool isInVolume = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,20 +20,55 @@ public class Node : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(isInVolume)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("E KEY " + transform.name);
+
+                Transform parent = transform.parent;
+                if(parent)
+                {
+                    Node node = parent.GetComponent<Node>();
+                    if (node)
+                    {
+                        foreach (Transform child in parent)
+                        {
+                            if (child != transform)
+                            {
+                                child.gameObject.SetActive(false);
+                                child.GetComponent<Node>().usable = false;
+                            }
+                                
+                        }
+                    }
+                }
+                
+            }
+
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
         
         if(other.tag == "Player")
         {
+            isInVolume = true;
+
             Debug.Log("Collider is " + other.name);
             if (textObj)
             {
                 textObj.gameObject.SetActive(true);
-                textObj.text = text;
+                textObj.text = text;                
             }
-                
+
+            foreach (Transform child in transform)
+            {
+                if(child.GetComponent<Node>().usable)
+                    child.gameObject.SetActive(true);
+            }
+
+            
         }
 
     }
@@ -38,6 +76,8 @@ public class Node : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            isInVolume = false;
+
             Debug.Log("Collider is " + other.name);
             if (textObj)
                 textObj.gameObject.SetActive(false);
